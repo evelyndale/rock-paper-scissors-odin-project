8a69137f6e0534eec0613a542ad6c39c1aa00963
Evelyn Dale <evelynvioletdale@gmail.com>
2022-09-15 22:47:45 -0700

    debugging errors with playRound()

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
-function getComputerChoice() {
-  const selector = Math.floor(Math.random() * 3);
-  if (selector === 0) {
-    return "Rock";
-  } else if (selector === 1) {
-    return "Paper";
-  } else if (selector === 2) {
-    return "Scissors";
-  } else {
-    return "Error, selector returned " + selector;
-  }
-}
+//prompt player for input and convert to lowercase
+const playerInputRaw = prompt("Enter your selection:");
+const playerSelection = playerInputRaw.toLowerCase();
 
+// assign output of computerChoice function to variable
 let computerSelection = getComputerChoice();
 
-const playerInput = prompt("Enter your selection:");
-
-console.log("Player selected " + playerInput);
+//testing that player and computer selections have been made
+console.log("Player selected " + playerSelection);
 console.log("computer selected " + computerSelection);
 
+//print the result of the game
+let result = playRound();
+console.log(result);
+
 function playRound(playerSelection, computerSelection) {
+  console.log(
+    "player selected " +
+      playerSelection +
+      " and computer selected " +
+      computerSelection
+  );
   if (
     (playerSelection == "rock" && computerSelection == "rock") ||
     (playerSelection == "scissors" && computerSelection == "scissors") ||
@@ -38,6 +39,19 @@ function playRound(playerSelection, computerSelection) {
   ) {
     return "computer wins! ☹";
   } else {
-    return "error";
+    return "error: could not compute winner. something aint right with function playRound";
+  }
+}
+
+function getComputerChoice() {
+  const selector = Math.floor(Math.random() * 3);
+  if (selector === 0) {
+    return "rock";
+  } else if (selector === 1) {
+    return "paper";
+  } else if (selector === 2) {
+    return "scissors";
+  } else {
+    return "Error, selector returned " + selector;
   }
 }
